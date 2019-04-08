ced48445f7e4758eea5361087210dbe15ad88cf5
alpesh-clairvoyantsoft <alpesh.patel@clairvoyantsoft.com>
2019-04-08 16:45:06 -0700

    first commit

---
+++ src/main/java/com/clairvoyant/app/MainApp.java
@@ -0,0 +1,12 @@
+//package com.clairvoyant.app;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class MainApp {
+//	
+//	public static void main(String[] args) throws Exception {
+//		SpringApplication.run(MainApp.class, args);
+//	}
+//
+//
+//}
